07e7f14a8edef3aacf4b2cd072d8141c8c9073bd
Lê Ngọc Minh Thư <116614977+lnmthu1505@users.noreply.github.com>
2023-10-14 17:07:25 +0700

    update request new weight col

--- shift_register.v
+++ shift_register.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/08/2023 12:19:33 PM
-// Design Name: 
-// Module Name: shift_register
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module shift_register 
 #(
     parameter BIT_WIDTH = 8,
@@ -36,15 +14,17 @@ module shift_register
     input en_shift,
     input [STRB_WIDTH-1:0] data_strobe,
     input [2*BIT_WIDTH*N_PIX_IN-1:0] data_in,
-    output [2*BIT_WIDTH*N_PIX_OUT-1:0] data_out,
-    output accumn_fin
+    output [2*BIT_WIDTH*N_PIX_OUT-1:0] data_out
 );
     localparam BIT_LEFT = 2*BIT_WIDTH*N_PIX_OUT - 2*BIT_WIDTH*N_COL_KERNEL;
 
     reg [2*BIT_WIDTH*N_PIX_OUT-1:0] tmp_data_out;
     reg [2*BIT_WIDTH*N_PIX_OUT-1:0] data_accumm;
     reg [2*BIT_WIDTH*N_COL_KERNEL-1:0] sub_data;
+    reg accum_fin_continue ;
 
+    wire [STRB_WIDTH-1:0] data_strobe;
+    wire reg_data;
 
     integer byte_id; //index
 
@@ -67,8 +47,7 @@ module shift_register
             end 
         end
     end
-    
-    integer cnt_accumm;
+
     always @(posedge clk, negedge rst_n) begin
         if(!rst_n) begin
             data_accumm <= 0;
@@ -86,5 +65,28 @@ module shift_register
         end
     end
 
-    assign accumn_fin = (cnt_accumm == N_COL_FEATURE-1) ? 1 : 0;
+    always @(posedge clk, negedge rst_n) begin
+        if(rst_n) begin
+            accum_fin_continue <= 0;
+            tmp_cnt_accum <= 0;
+        end
+        else begin
+            if(req_data) begin
+                accum_fin_continue <= 1;
+                tmp_cnt_accum <= tmp_cnt_accum + 1;
+            end 
+            else begin
+                if(tmp_cnt_accum != NO_COL_KERNEL-1) begin
+                    accum_fin_continue <= 1;
+                    tmp_cnt_accum <= tmp_cnt_accum + 1;
+                end
+                else begin
+                    accum_fin_continue <= 0;
+                    tmp_cnt_accum <= 0;
+                end
+            end
+        end
+    end
+    assign req_data = (cnt_accumm == N_COL_FEATURE-1) ? 1 : 0
+    assign accumn_fin = req_data | accum_fin_continue;
 endmodule
